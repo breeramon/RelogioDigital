d12bd4665d42b66695dbe5fc17686e5cd5e6479b
breeramon <117483744+breeramon@users.noreply.github.com>
2024-01-18 13:11:00 +0000

    correção nos segundos do relógio

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ const relogio = setInterval(function time(){
 
   if (min < 10) min = '0' + min;
 
-  if (seg < 10) se = '0' + seg;
+  if (seg < 10) seg = '0' + seg;
 
   horas.textContent = hora;
   minutos.textContent = min;
